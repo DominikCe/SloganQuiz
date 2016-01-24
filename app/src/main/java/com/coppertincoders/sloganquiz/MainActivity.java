cac55e811cc94f4a61ba5683fe16f15bda6d4bbf
SpaghettiDemon <knobi98@hotmail.de>
2016-01-24 16:53:34 +0100

    aassdasd

--- app/src/main/java/com/coppertincoders/sloganquiz/MainActivity.java
+++ app/src/main/java/com/coppertincoders/sloganquiz/MainActivity.java
@@ -5,12 +5,17 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Layout;
+import android.util.Xml;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import org.xml.sax.XMLReader;
+import org.xmlpull.v1.XmlPullParser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         txtInput = (EditText) findViewById(R.id.txtInput);
         buSubmit = (Button) findViewById(R.id.buSubmit);
 
+
         sSolution = "Bauhaus";
 
         //Submit-button OnClick-Event
@@ -52,6 +58,10 @@ public class MainActivity extends AppCompatActivity {
         } else {
             lblSlogan.setText("FALSCH!!!");
         }
+    }
+
+    public void loadSlogan() {
+        
     }
 
     @Override
